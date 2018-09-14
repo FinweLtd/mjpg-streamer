784a511a5d5178d56010045ae42f4df65c3bcd61
Tapani Rantakokko <tapani.rantakokko@finwe.fi>
2018-09-14 08:37:57 +0000

    updated powerkey watchdog script

--- scripts/watchdog_powerkey.sh
+++ scripts/watchdog_powerkey.sh
@@ -5,7 +5,8 @@
 # login daemon: sudo nano /etc/systemd/logind.conf where you
 # need to set HandlePowerKey=ignore
 
-POWERKEY_CMD="evtest /dev/input/event0 | unbuffer -p grep \"value 1\" | /home/odroid/handle_powerkey.sh"
+EVENT="$(ls -l /dev/input/by-path | grep gpio | grep -Eo 'event[0-9]+')"
+POWERKEY_CMD="evtest /dev/input/${EVENT} | unbuffer -p grep \"value 1\" | /home/odroid/handle_powerkey.sh"
 
 echo "Power key watchdog starting"
 bash -c "$POWERKEY_CMD" &
